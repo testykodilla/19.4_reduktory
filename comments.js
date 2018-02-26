7978a4658cd5dc6e937fc8d579b22b1b8c4a197f
testykodilla <32685259+testykodilla@users.noreply.github.com>
2018-02-26 19:11:36 +0100

    Update comments.js

--- comments.js
+++ comments.js
@@ -1,48 +1,46 @@
+import {ADD_COMMENT} from './actions';
+import {REMOVE_COMMENT} from './actions';
+import {EDIT_COMMENT} from './actions';
+import {THUMB_UP_COMMENT} from './actions';
+import {THUMB_DOWN_COMMENT} from './actions';
 
-function reducer(state = initialState, action) {
+function comments(state = [], action) {
     switch(action.type) {
         case ADD_COMMENT:
-            return Object.assign({}, state, {
-                comments: [
-                {
-                    id: action.id,
-                    text: action.text,
-                }
-                , ...state]
-            });
+            return [
+                    {
+                        id: action.id,
+                        text: action.text,
+                        votes: 0
+                    }, 
+                    ...state,
+                ];
         case REMOVE_COMMENT:
-            return Object.assign({}, state, {
-                comments: state.comments.filter(comment => comment.id !== action.id)
-            });
+            return  state.filter(comment => comment.id !== action.id);
         case EDIT_COMMENT:
-            return Object.assign({}, state, {
-                comments: [
-                {
-                    id: action.id,
-                    text: action.text,
+            return state.map(comment => {
+                if (comment.id === action.id) {
+                    return {...comment, text: action.text}
                 }
-                , ...state]
+                return comment;
             });
-        case THUMB_UP_COMMENT: 
-            return Object.assign({}, state, {
-                comments: [
-                {
-                    id: action.id,
-                    votes = votes +=1,
+        case THUMB_UP_COMMENT:
+            return state.map(comment => {
+                if (comment.id === action.id) {
+                    return {...comment, votes: comment.votes + 1}
                 }
-                , ...state]
+                return comment;
             });
-            case THUMB_DOWN_COMMENT: 
-            return Object.assign({}, state, {
-                comments: [
-                {
-                    id: action.id,
-                    votes = votes -=1,
+        case THUMB_DOWN_COMMENT:
+            return state.map(comment => {
+                if (comment.id === action.id) {
+                    return {...comment, votes: comment.votes - 1}
                 }
-                , ...state]
+                return comment;
             });
-            default:
+        default:
             return state;
-    }
+    } 
+}
 
-};
+export default comments;
